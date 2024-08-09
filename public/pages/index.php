1796a2a9cc72ced5f49cee48e04eb54169f0792a
victorcsar <victorcesagx@gmail.com>
2024-08-08 22:14:54 -0300

    Estruturas das pastas e criacao da tela de login

---
+++ public/pages/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <!-- <link rel="stylesheet" href="../../assets/css/index.css"> -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">  
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row justify-content-center align-items-center min-vh-100">
+            <div class="col-md-5">
+                <div class="bg-dark text-white p-4 rounded">
+                    <h1 class="text-center mb-3 ">Logar no Sistema</h1>
+                    <form>
+                        <div class="mb-4">
+                            <input type="text" class="form-control" placeholder="Nome">
+                        </div>
+                        <div class="mb-4">
+                            <input type="password" class="form-control" placeholder="Senha">
+                        </div>
+                        <div class="text-center">
+                            <button type="submit" class="btn btn-primary w-50">Enviar</button> 
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+
+</html>
